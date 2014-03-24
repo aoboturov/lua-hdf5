1d2228560177e1df4309f43afcaed9eb9b2ae8e9
Peter Colberg <peter@colberg.org>
2014-03-24 14:16:29 -0400

    FFI C: fix missing assembler names with -O2

--- ffi-cdecl/ffi-cdecl.h
+++ ffi-cdecl/ffi-cdecl.h
@@ -8,7 +8,7 @@
 #define cdecl_struct(tag)               void cdecl_struct__ ## tag(struct tag *unused) {}
 #define cdecl_union(tag)                void cdecl_union__ ## tag(union tag *unused) {}
 #define cdecl_enum(tag)                 void cdecl_enum__ ## tag(enum tag *unused) {}
-#define cdecl_func(id)                  void cdecl_expr__ ## id(__typeof__(id) unused) { cdecl_expr__ ## id(id); }
+#define cdecl_func(id)                  void cdecl_func__ ## id(__typeof__(id)); void cdecl_expr__ ## id() { cdecl_func__ ## id(id); }
 #define cdecl_var                       cdecl_func
 #define cdecl_const                     cdecl_func
 
