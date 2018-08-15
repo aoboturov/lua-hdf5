81d2380edf27bf37fb7b41895b648a174a6b50cd
Moritz Maxeiner <moritz@ucworks.org>
2018-08-15 12:59:48 +0200

    Drop custom merged version of gcc-lua and gcc-lua-cdecl

--- gcc-lua-cdecl/test/variable.c
+++
@@ -1,9 +0,0 @@
-/*
- * C declaration composer for the GCC Lua plugin.
- * Copyright © 2013–2015 Peter Colberg.
- * Distributed under the MIT license. (See accompanying file LICENSE.)
- */
-
-/* GCC C extension: variable declaration attributes */
-extern volatile short extern_aligned_volatile_short __attribute__((weak, aligned(32)));
-volatile short *ptr_to_extern_aligned_volatile_short = &extern_aligned_volatile_short;
--- gcc-lua/gcc/gcclua-config.h
+++
@@ -1,6 +0,0 @@
-#include "gcc-plugin.h"
-#include "plugin-version.h"
-
-#if GCCPLUGIN_VERSION >= 4008 || defined(ENABLE_BUILD_WITH_CXX)
-#define GCCLUA_USE_CXX
-#endif
